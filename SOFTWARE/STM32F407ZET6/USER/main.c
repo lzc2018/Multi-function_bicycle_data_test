19eae40eb371824eca537076d3a7d6543ea00b27
lzc2018 <1633502122@qq.com>
2018-03-04 19:17:55 +0800

    添加LED

--- SOFTWARE/STM32F407ZET6/USER/main.c
+++ SOFTWARE/STM32F407ZET6/USER/main.c
@@ -3,21 +3,36 @@
 #include "delay.h"
 #include "sys.h"
 #include "LED.h"
+#include "key.h"
 
-int main(void)
-{
-	LED_Init();
-	delay_init(168);
+ int main(void)
+ {
+ 	vu8 key=0;	
+	delay_init(168);	    	 //延时函数初始化	  
+ 	LED_Init();			     //LED端口初始化
+	KEY_Init();          //初始化与按键连接的硬件接口
 	
+	LED0=0;					//先点亮红灯
 	while(1)
 	{
-		GPIO_ResetBits(GPIOF,GPIO_Pin_9);
-		GPIO_SetBits(GPIOF,GPIO_Pin_10);
-		delay_ms(500);
-		
-		GPIO_ResetBits(GPIOF,GPIO_Pin_10);
-		GPIO_SetBits(GPIOF,GPIO_Pin_9);
-		delay_ms(500);
-	}
-	
+ 		key=KEY_Scan(0);	//得到键值
+	   	if(key)
+		{						   
+			switch(key)
+			{				 
+				
+				case KEY2_PRES:	//控制LED0翻转
+					LED0=!LED0;
+					break;
+				case KEY1_PRES:	//控制LED1翻转	 
+					LED1=!LED1;
+					break;
+				case KEY0_PRES:	//同时控制LED0,LED1翻转 
+					LED0=!LED0;
+					LED1=!LED1;
+					break;
+			}
+		}else delay_ms(10); 
+	}	 
 }
+
