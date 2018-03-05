7c0327f77e0d1f56e57c47b96a996e97659fca71
lzc2018 <1633502122@qq.com>
2018-03-05 15:33:19 +0800

    添加ILI9488

--- SOFTWARE/STM32F407ZET6/USER/main.c
+++ SOFTWARE/STM32F407ZET6/USER/main.c
@@ -1,38 +1,49 @@
-#include "stm32f4xx.h"
-#include "usart.h"
-#include "delay.h"
 #include "sys.h"
-#include "LED.h"
-#include "key.h"
+#include "delay.h"
+#include "usart.h"
+#include "led.h"
+#include "lcd.h"
 
- int main(void)
- {
- 	vu8 key=0;	
-	delay_init(168);	    	 //延时函数初始化	  
- 	LED_Init();			     //LED端口初始化
-	KEY_Init();          //初始化与按键连接的硬件接口
+
+
+int main(void)
+{ 
+ 	u8 x=0;
+	u8 lcd_id[12];				//存放LCD ID字符串
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);//设置系统中断优先级分组2
+	delay_init(168);      //初始化延时函数
+	uart_init(115200);		//初始化串口波特率为115200
 	
-	LED0=0;					//先点亮红灯
-	while(1)
-	{
- 		key=KEY_Scan(0);	//得到键值
-	   	if(key)
-		{						   
-			switch(key)
-			{				 
-				
-				case KEY2_PRES:	//控制LED0翻转
-					LED0=!LED0;
-					break;
-				case KEY1_PRES:	//控制LED1翻转	 
-					LED1=!LED1;
-					break;
-				case KEY0_PRES:	//同时控制LED0,LED1翻转 
-					LED0=!LED0;
-					LED1=!LED1;
-					break;
-			}
-		}else delay_ms(10); 
-	}	 
+	LED_Init();					  //初始化LED
+ 	LCD_Init();           //初始化LCD FSMC接口
+	POINT_COLOR=RED;      //画笔颜色：红色
+	sprintf((char*)lcd_id,"LCD ID:%04X",lcddev.id);//将LCD ID打印到lcd_id数组。				 	
+  	while(1) 
+	{		 
+		switch(x)
+		{
+			case 0:LCD_Clear(WHITE);break;
+			case 1:LCD_Clear(BLACK);break;
+			case 2:LCD_Clear(BLUE);break;
+			case 3:LCD_Clear(RED);break;
+			case 4:LCD_Clear(MAGENTA);break;
+			case 5:LCD_Clear(GREEN);break;
+			case 6:LCD_Clear(CYAN);break; 
+			case 7:LCD_Clear(YELLOW);break;
+			case 8:LCD_Clear(BRRED);break;
+			case 9:LCD_Clear(GRAY);break;
+			case 10:LCD_Clear(LGRAY);break;
+			case 11:LCD_Clear(BROWN);break;
+		}
+		POINT_COLOR=RED;	  
+		LCD_ShowString(30,40,210,24,24,"Explorer STM32F4");	
+		LCD_ShowString(30,70,200,16,16,"TFTLCD TEST");
+		LCD_ShowString(30,90,200,16,16,"ATOM@ALIENTEK");
+ 		LCD_ShowString(30,110,200,16,16,lcd_id);		//显示LCD ID	      					 
+		LCD_ShowString(30,130,200,12,12,"2014/5/4");	      					 
+	  x++;
+		if(x==12)x=0;
+		LED0=!LED0;	 
+		delay_ms(1000);	
+	} 
 }
-
