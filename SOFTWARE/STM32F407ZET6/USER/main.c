d2124b5e9350dd009c0b112909d15bcfce95f9bc
lzc2018 <1633502122@qq.com>
2018-03-06 22:16:05 +0800

    RTC时钟

--- SOFTWARE/STM32F407ZET6/USER/main.c
+++ SOFTWARE/STM32F407ZET6/USER/main.c
@@ -4,10 +4,17 @@
 #include "led.h"
 #include "lcd.h"
 #include "adc.h"
+#include "rtc.h"
 
 
 int main(void)
 { 
+	
+	RTC_TimeTypeDef RTC_TimeStruct;
+	RTC_DateTypeDef RTC_DateStruct;
+	
+	u8 tbuf[40];
+	u8 t=0;
  	u8 x=0;
 	short temp; 
 	u8 lcd_id[12];				//存放LCD ID字符串
@@ -17,7 +24,11 @@ int main(void)
 	
 	LED_Init();					  //初始化LED
  	LCD_Init();           //初始化LCD FSMC接口
-	Adc_Init();         //内部温度传感器ADC初始化
+	Adc_Init();  	//内部温度传感器ADC初始化
+	My_RTC_Init();		 		//初始化RTC
+	
+	RTC_Set_WakeUp(RTC_WakeUpClock_CK_SPRE_16bits,0);		//配置WAKE UP中断,1秒钟中断一次
+	
 	POINT_COLOR=RED;      //画笔颜色：红色
 	sprintf((char*)lcd_id,"LCD ID:%04X",lcddev.id);//将LCD ID打印到lcd_id数组。				 	
   	while(1) 
@@ -50,6 +61,24 @@ int main(void)
 	LCD_ShowString(30,140,200,16,16,"TEMPERATE: 00.00C");//先在固定位置显示小数点	      
 	while(1)
 		{
+			t++;
+			
+			if((t%10)==0)	//每100ms更新一次显示数据
+		{
+;
+			RTC_GetTime(RTC_Format_BIN,&RTC_TimeStruct);
+			
+			sprintf((char*)tbuf,"Time:%02d:%02d:%02d",RTC_TimeStruct.RTC_Hours,RTC_TimeStruct.RTC_Minutes,RTC_TimeStruct.RTC_Seconds); 
+			LCD_ShowString(30,140,210,16,16,tbuf);	
+			
+			RTC_GetDate(RTC_Format_BIN, &RTC_DateStruct);
+			
+			sprintf((char*)tbuf,"Date:20%02d-%02d-%02d",RTC_DateStruct.RTC_Year,RTC_DateStruct.RTC_Month,RTC_DateStruct.RTC_Date); 
+			LCD_ShowString(30,160,210,16,16,tbuf);	
+			sprintf((char*)tbuf,"Week:%d",RTC_DateStruct.RTC_WeekDay); 
+			LCD_ShowString(30,180,210,16,16,tbuf);
+		} 
+			
 			temp=Get_Temprate();	//得到温度值 
 			if(temp<0)
 			{
@@ -59,9 +88,10 @@ int main(void)
 		
 			LCD_ShowxNum(30+11*8,140,temp/100,2,16,0);		//显示整数部分
 			LCD_ShowxNum(30+14*8,140,temp%100,2,16,0);		//显示小数部分 
-		
+			
+			if((t%20)==0)	//每200ms,翻转一次LED0 
 			LED0=!LED0;	 
-			delay_ms(1000);	
+			delay_ms(10);	
 		} 
 	}
 }
